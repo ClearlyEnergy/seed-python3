f8bb5a0472e06eea12d783bfc19dc57942095c77
Kyle Robbertze <kyle@paddatrapper.com>
2019-12-06 16:05:32 +0200

    Revert "fix migration ordering"
    This reverts commit 4a692fa1d433abf850d53c99114997fcd0dafe9f.

--- seed/migrations/0114_auto_20191202_1652.py
+++ seed/migrations/0114_auto_20191202_1652.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('seed', '0115_backfill_organization_columns'),
+        ('seed', '0113_column_geocoding_order'),
     ]
 
     operations = [
