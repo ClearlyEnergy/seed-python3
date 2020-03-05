70080a59380da84a155684fa39ae2a368281a4aa
ClearlyEnergy <vbugnion@clearlyenergy.com>
2020-03-05 09:10:44 -0500

    reverted seed/landing/models.py changes

--- seed/landing/models.py
+++ seed/landing/models.py
@@ -101,10 +101,11 @@ class SEEDUser(AbstractBaseUser, PermissionsMixin):
         if not auth_header:
             return None
 
-        if auth_header.startswith('Bearer'):
-            return None
+#        if auth_header.startswith('Bearer'):
+#            return None
 
-        if not getattr(request, 'user', None):
+#        if not getattr(request, 'user', None):
+        if not auth_header.startswith('Bearer') or not getattr(request, 'user', None):
             try:
                 if not auth_header.startswith('Basic'):
                     raise exceptions.AuthenticationFailed('Only Basic HTTP_AUTHORIZATION is supported')
@@ -116,14 +117,16 @@ class SEEDUser(AbstractBaseUser, PermissionsMixin):
 
                 valid_api_key = re.search('^[a-f0-9]{40}$', api_key)
                 if not valid_api_key:
-                    return None
+                    raise exceptions.AuthenticationFailed('Invalid API key')
+                    # return None
 
                 user = SEEDUser.objects.get(api_key=api_key, username=username)
                 return user
             except ValueError:
                 raise exceptions.AuthenticationFailed('Invalid HTTP_AUTHORIZATION Header')
             except SEEDUser.DoesNotExist:
-                return None
+                raise exceptions.AuthenticationFailed('Invalid API key')
+                # return None
 
     def get_absolute_url(self):
         return "/users/%s/" % urlquote(self.username)
